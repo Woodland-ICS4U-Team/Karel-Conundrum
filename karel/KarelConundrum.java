553fefc4667077f22b4a4c6a10928786fcd5c2a8
Thomas Graham-Binkowski <lordpotatopancake@gmail.com>
2014-11-24 13:41:23 -0500

    Trying to test andrews stuff.

--- karel/KarelConundrum.java
+++ karel/KarelConundrum.java
@@ -2,36 +2,45 @@ import becker.robots.*;
 import java.util.*;
 
 public class KarelConundrum {
-    public static void main(String[] args) {
-        Random randx = new Random();
-        Random randy = new Random();
+	public static void main(String[] args) {
+		Random randx = new Random();
+		Random randy = new Random();
 
-        int x = randx.nextInt(250)+1;
-        int y = randy.nextInt(250)+1;
+		int x = randx.nextInt(250)+1;
+		int y = randy.nextInt(250)+1;
 
-        City Conundrum = new City(250,250);
-        Thing t1 = new Thing(Conundrum, x, y);
-        /*
-        Robot jacob = new Robot(toronto, 0, 0, Direction.WEST, 0);
-        Robot matt = new Robot(toronto, 0, 0, Direction.WEST, 0);
-        Robot andrew = new Robot(toronto, 0, 0, Direction.WEST, 0);
-        Robot caleb = new Robot(toronto, 0, 0, Direction.WEST, 0);
-        */
+		City conundrum = new City(250,250);
+		Thing t1 = new Thing(conundrum, x, y);
 
-        Thread jacobsThread = new Thread() {
+		AndrewRobot andrew = new AndrewRobot(conundrum, 0, 0, Direction.WEST, 0);
+/*
+		jacobsRobot jacob = new Robot(conundrum, 0, 0, Direction.WEST, 0);
+		mattsRobot matt = new Robot(conundrum, 0, 0, Direction.WEST, 0);
+		andrewsRobot andrew = new Robot(conundrum, 0, 0, Direction.WEST, 0);
+		calebsRobot caleb = new Robot(conundrum, 0, 0, Direction.WEST, 0);
+*/
+/*
+		Thread jacobsThread = new Thread() {
 
-        };
+		};
 
-        Thread mattsThread = new Thread() {
+		Thread mattsThread = new Thread() {
 
-        };
+		};
 
-        Thread andrewsThread = new Thread() {
+		Thread andrewsThread = new Thread() {
 
-        };
+		};
 
-        Thread calebsThread = new Thread() {
+		Thread calebsThread = new Thread() {
 
-        };
-    }
+		};
+
+		jacobsThread.start();
+		mattsThread.start();
+		andrewsThread.start();
+		calebsThread.start();
+*/
+		andrew.Find();
+	}
 }
