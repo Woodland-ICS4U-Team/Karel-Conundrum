076647a3aef1d8362015f48e770571a3494c8ff6
Thomas Graham-Binkowski <lordpotatopancake@gmail.com>
2014-11-24 13:15:07 -0500

    Created the city

--- karel/KarelConundrum.java
+++ karel/KarelConundrum.java
@@ -1,8 +1,15 @@
 import becker.robots.*;
+import java.util.*;
 
 public class KarelConundrum {
 	public static void main(String[] args) {
+		Random randx = new Random();
+		Random randy = new Random();
+
+		int x = randx.nextInt(250)+1;
+		int y = randy.nextInt(250)+1;
+
 		City Conundrum = new City(250,250);
-		Thing t1 = new Thing(Conundrum, 7, 0);
+		Thing t1 = new Thing(Conundrum, x, y);
 	}
 }
