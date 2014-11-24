fe2f8deb6b8f3f6d4e25511cec4daef974e2daba
Zentac42 <AndrVeld@woodland.on.ca>
2014-11-24 13:56:50 -0500

    My own

--- karel/AndrewRobot.java
+++ karel/AndrewRobot.java
@@ -2,22 +2,13 @@
 import becker.robots.*;
 public class andrewRobot extends KarelConundrum {
     private int distFromEdge = 0;
-
+    public AndrewRobot () {
+        super();
+    }
     public boolean andFind() {
         int x = 0;
         int y = 0;
-        for (y = 0; y <= 250; y++) {
-            for (x = 0; x <= 250; x++) {
-                super.move();
-            }
-            super.turnLeft();
-            super.turnLeft();
-            super.turnLeft();
-            super.move();
-            super.turnLeft();
-            super.turnLeft();
-            super.turnLeft();
-        }
+
     }
     public boolean find() {
         return true;
