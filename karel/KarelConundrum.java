688baa78e726c525000088fbef1536fb6154572c
Zentac42 <AndrVeld@woodland.on.ca>
2014-11-24 13:28:42 -0500

    changed names of robots

--- karel/KarelConundrum.java
+++ karel/KarelConundrum.java
@@ -2,36 +2,36 @@ import becker.robots.*;
 import java.util.*;
 
 public class KarelConundrum {
-	public static void main(String[] args) {
-		Random randx = new Random();
-		Random randy = new Random();
+    public static void main(String[] args) {
+        Random randx = new Random();
+        Random randy = new Random();
 
-		int x = randx.nextInt(250)+1;
-		int y = randy.nextInt(250)+1;
+        int x = randx.nextInt(250)+1;
+        int y = randy.nextInt(250)+1;
 
-		City Conundrum = new City(250,250);
-		Thing t1 = new Thing(Conundrum, x, y);
-/*
-		jacobsRobot jacob = new Robot(toronto, 0, 0, Direction.WEST, 0);
-		mattsRobot matt = new Robot(toronto, 0, 0, Direction.WEST, 0);
-		andrewsRobot andrew = new Robot(toronto, 0, 0, Direction.WEST, 0);
-		calebsRobot caleb = new Robot(toronto, 0, 0, Direction.WEST, 0);
-*/
+        City Conundrum = new City(250,250);
+        Thing t1 = new Thing(Conundrum, x, y);
+        /*
+        Robot jacob = new Robot(toronto, 0, 0, Direction.WEST, 0);
+        Robot matt = new Robot(toronto, 0, 0, Direction.WEST, 0);
+        Robot andrew = new Robot(toronto, 0, 0, Direction.WEST, 0);
+        Robot caleb = new Robot(toronto, 0, 0, Direction.WEST, 0);
+        */
 
-		Thread jacobsThread = new Thread() {
+        Thread jacobsThread = new Thread() {
 
-		};
+        };
 
-		Thread mattsThread = new Thread() {
+        Thread mattsThread = new Thread() {
 
-		};
+        };
 
-		Thread andrewsThread = new Thread() {
+        Thread andrewsThread = new Thread() {
 
-		};
+        };
 
-		Thread calebsThread = new Thread() {
+        Thread calebsThread = new Thread() {
 
-		};
-	}
+        };
+    }
 }
