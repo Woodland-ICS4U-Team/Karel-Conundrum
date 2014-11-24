9031348d0cdc210343d127ebb7f10d3d2fe8890a
Zentac42 <AndrVeld@woodland.on.ca>
2014-11-24 13:25:03 -0500

    Made my method

---
+++ karel/AndrewRobot.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class AndrewRobot here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import becker.robots.*;
+public class AndrewRobot extends KarelConundrum
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    
+    /**
+     * Constructor for objects of class AndrewRobot
+     */
+    public static void andrewFind()
+    {
+        randx = 5;
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
