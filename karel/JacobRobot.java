a368660a2576015d381c6deff5ffd631da8cceb1
Jacob Brunsting <jacob.brunsting@gmail.com>
2014-11-24 13:38:18 -0500

    Modified Jacob

--- karel/JacobRobot.java
+++ karel/JacobRobot.java
@@ -1,11 +1,18 @@
 import becker.robots.*;
 public class JacobRobot extends Robot {
     private int distFromEdge = 0;
+    private int citySize = 250;
     public JacobRobot (City cs, int ss, int as, Direction ds, int bs) {    
         super(cs, ss, as, ds, bs);
     }
     
     public boolean find() {
+        while (super.getDirection() != Direction.EAST) {
+            super.turnLeft();
+        }
+        while (!super.canPickThing()) {
+            super.move();
+        }
         return true;
     }
 }
