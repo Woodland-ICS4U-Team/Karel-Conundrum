13a5c19d6695927bedffdd3bab5978276a8199c5
Zentac42 <AndrVeld@woodland.on.ca>
2014-11-25 10:09:07 -0500

    Changed my robot file

--- karel/AndrewRobot.java
+++ karel/AndrewRobot.java
@@ -1,16 +1,41 @@
 
 import becker.robots.*;
 public class andrewRobot extends KarelConundrum {
-    private int distFromEdge = 0;
-    public AndrewRobot () {
-        super();
+    public AndrewRobot (City cs, int ss, int as, Direction ds, int bs) {    
+        super(cs, ss, as, ds, bs);
     }
-    public boolean andFind() {
-        int x = 0;
-        int y = 0;
+    public static boolean find() {
+        int cnt = 0;
+        Direction d = super.getDirection();
+        boolean b = super.canPickThing();
+        boolean finish = false;
+        int x = 100;
+        Thing t1 = new Thing(toronto, 25, 10);
+        while (d != Direction.SOUTH) {
+            super.turnLeft();
+            d = super.getDirection();
+            
+        }
+        cnt++;
+        while (!finish) {
+            for (int i = x; i > 0 ; i--) {
+                super.move();
+                b = super.canPickThing();
+                if (b == true) {
+                    super.pickThing();
+                    finish = true;
+                    i = 0;
+                }
+            }
+            super.turnLeft();
+            cnt++;
+            if (cnt == 2) {
+                x--;
+                cnt = 0;
+            }
+            
+        }
         
-    }
-    public boolean find() {
         return true;
     }
    
